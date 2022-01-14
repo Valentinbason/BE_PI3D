c16136cb24698ec1ecec908c4906532ac3f533f3
Valentinbason <valentin.bason@hotmail.com>
2022-01-14 16:21:40 +0100

    maj

--- base_apprentissage.m
+++ base_apprentissage.m
@@ -6,31 +6,37 @@ for k=1:1
     figure;
     [X,Y,Z] = peaks(51);
     A = surf(X,Y,Z);
-    Z1 = Z;
     colormap('gray');
-    c = colorbar;
-    l = c.Limits;
-    Z = Z + abs(l(1));
-    l = l + abs(l(1));
-    Z = flipud(Z/l(2));
-    Im = uint8(Z*255);
+%     c = colorbar;
+%     l = c.Limits;
+%     Z = Z + abs(l(1));
+%     l = l + abs(l(1));
+%     Z = flipud(Z/l(2));
+%     Im = uint8(Z*255);
+%     figure;
+%     imshow(Im);
+    
+    % Récupération des normales
+    normals = reshape(A.FaceNormals,[50*50 3]);
+    normals = normals./sqrt(sum(normals.^2,2));
+    normals = reshape(normals,[50 50 3]);
+    % Image en fonction des normales
+    Im = flipud(uint8(normals(:,:,3)*255));
     figure;
     imshow(Im);
-
-    normals = [];
-    for i=1:49
-        for j = 1:49
-            imagette = Z1(i:i+2,j:j+2);
-            normals = [normals; A.FaceNormals(i+1,j+1,:)];
-%             l = c.Limits;
-%             imagette = imagette + abs(l(1));
-%             l = l + abs(l(1));
-%             imagette = flipud(imagette/l(2));
-%             imagette = uint8(imagette*255);
-%             imwrite(imagette,strcat('im',int2str(sub2ind([49,49],i,j)),'.png'),'png');
+    
+    % Récuperation des imagettes
+    for j=1:42
+        for i=1:42
+            imagette = Im(i:i+8,j:j+8);
+            imwrite(imagette,strcat('Data/imagettes_8x8/im',int2str(sub2ind([42,42],i,j)),'.png'),'png');
         end
     end
 
+    % Supression des normales inutiles
+    normals([1 2 3 4 47 48 49 50],:,:) = [];
+    normals(:,[1 2 3 4 47 48 49 50],:) = [];
+    normals = reshape(normals,[42*42 3]);
 end
 
-save('normals.mat','normals');
+save('Data/imagettes_8x8/normals.mat','normals');
